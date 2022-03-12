1dca2686b37090918a4056d30e63afa5f6cc77b5
codeer <loginonlyyy@gmail.com>
2022-03-12 16:18:30 +0900

    start project

---
+++ Components/NavBar.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="about">
+        <a>About</a>
+      </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+      `}</style>
+    </nav>
+  );
+}
